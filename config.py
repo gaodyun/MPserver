edf3a7654751f5b2c1aae7cf386086418c0a233d
Mr.Gao <gaodyun@gmail.com>
2018-03-19 14:42:41 +0800

    the config file for werobot, such as appid, token ...

---
+++ config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+TOKEN = 'yourtoken'
+APP_ID = 'yourappid'
+APP_SECRET = 'yourappsecret'
+ENCODING_AES_KEY = 'yourencodingaeskey'
+ENABLE_SESSION = False
+SESSION_STORAGE = False
